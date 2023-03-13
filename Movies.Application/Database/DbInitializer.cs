0f73ba00bfb7fe67b624347071e4661c235ca18b
Dennie Christensen <dch@commentor.dk>
2023-03-13 21:07:41 +0100

    Added seed data

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -1,15 +1,20 @@
 using System.Data;
+using System.Text.Json;
 using Dapper;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
 
 namespace Movies.Application.Database;
 
 public class DbInitializer
 {
     private readonly IDbConnectionFactory _dbConnectionFactory;
+    private readonly IMovieRepository _movieRepository;
     
-    public DbInitializer(IDbConnectionFactory dbConnectionFactory)
+    public DbInitializer(IDbConnectionFactory dbConnectionFactory, IMovieRepository movieRepository)
     {
         _dbConnectionFactory = dbConnectionFactory;
+        _movieRepository = movieRepository;
     }
 
     public async Task InitializeAsync()
@@ -43,5 +48,44 @@ public class DbInitializer
                 rating int NOT NULL,
                 PRIMARY KEY (userId, movieId));
         """);
+
+
+        await EnsureData();
+    }
+
+    private async Task EnsureData()
+    {
+        var existingMovies = await _movieRepository.GetAllAsync(new()
+        {
+            UserId = null,
+            Title = null,
+            YearOfRelease = null
+        });
+
+        var newMovies = GetMoviesData().Where(m => existingMovies.All(em => em.Id != m.Id)).Take(100).ToList();
+
+        if (!newMovies.Any())
+            return;
+        
+        
+        Console.WriteLine($"Creating {newMovies.Count} movies ...");
+        
+        foreach (var movie in newMovies)
+        {
+            await _movieRepository.CreateAsync(movie);
+            Console.WriteLine($"Created movie: {movie.Title}, {movie.YearOfRelease}");
+        }
+    }
+
+    private static IEnumerable<Movie> GetMoviesData()
+    {
+        var current = Directory.GetCurrentDirectory();
+        var src = Directory.GetParent(current)!.FullName;
+        var path = Path.Combine(src, "Helpers", "movies.json");
+        
+        var data = File.ReadAllText(path);
+        var movies =  JsonSerializer.Deserialize<IEnumerable<Movie>>(data);
+
+        return movies ?? Enumerable.Empty<Movie>();
     }
 }
